ecf4c6c6a68e8c976a789901a572a5a75fd3bb87
Annaliskirwa <annaliskirwa@gmail.com>
2021-10-10 10:12:57 +0300

    It will add the new quotes object

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -1,15 +1,51 @@
 import { Component, OnInit } from '@angular/core';
+import { Quote } from '../quote';
 
 @Component({
   selector: 'app-quote',
   templateUrl: './quote.component.html',
-  styleUrls: ['./quote.component.css']
+  styleUrls: ['./quote.component.scss']
 })
 export class QuoteComponent implements OnInit {
+  quotes = [
+    new Quote("MLK", "Martin Luther King", "Injustice anywhere is a threat to justice everywhere."),
+    new Quote("Life", "Margeret Atwood", "In the end we all become stories ")
+  ]
+    
 
-  constructor() { }
-
-  ngOnInit(): void {
+  addQuote(chosenQuote) {
+    this.quotes.push(chosenQuote)
   }
+
+  upvote(i) {
+    this.quotes[i].upvotes ++;
+  }
+  downvote(i) {
+    this.quotes[i].downvotes  ++;
+  }
+  delQuote(i) {
+    this.quotes.splice(i, 1)
+  }
+  preNum:number
+  lastNum:number
+  counter:number
+
+  highestUpvote(){
+    this.preNum = 0
+    this.lastNum = 0
+
+    for(this.counter=0 ; this.counter < this.quotes.length; this.counter++) {
+      this.lastNum = this.quotes[this.counter].upvotes;
+      if(this.lastNum > this.preNum){this.preNum = this.lastNum}
+    }
+    return  this.preNum
+  }
+
+
+
+constructor() { }
+
+ngOnInit() {
+}
 
 }
