cf1343c17e67449b7e9b811337eb9e08e77ee529
Annaliskirwa <annaliskirwa@gmail.com>
2021-10-10 11:14:00 +0300

    It will add a background color to quotes with high votes

--- src/app/highlight.directive.ts
+++ src/app/highlight.directive.ts
@@ -1,10 +1,10 @@
-import { Directive } from '@angular/core';
 
+import { Directive, ElementRef } from '@angular/core';
 @Directive({
   selector: '[appHighlight]'
 })
 export class HighlightDirective {
-
-  constructor() { }
-
+  constructor(elem:ElementRef) {
+    elem.nativeElement.style.backgroundColor="#834c69"
+   }
 }
