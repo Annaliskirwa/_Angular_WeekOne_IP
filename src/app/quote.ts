5b66abb337267121682df566d32b7d6532e1dc67
Annaliskirwa <annaliskirwa@gmail.com>
2021-10-10 10:18:28 +0300

    It will create a constructor for the name, author and quote

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,2 +1,11 @@
 export class Quote {
-}
+    public upvotes:number
+    public downvotes:number
+    public completeDate:Date
+    constructor(public name:string, public author:string,public quote:string){
+      this.upvotes =0;
+      this.downvotes =0;
+      this.completeDate=new Date;
+    }
+  }
+  
