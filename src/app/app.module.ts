d0ec2524d864d02a89a5b79cbdf99d29dd311b24
Annaliskirwa <annaliskirwa@gmail.com>
2021-10-10 10:17:27 +0300

    It will add a quotes class

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { MDBBootstrapModule } from 'angular-bootstrap-md';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { QuoteComponent } from './quote/quote.component';
@@ -14,7 +13,6 @@ import { QuoteComponent } from './quote/quote.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    MDBBootstrapModule.forRoot(),
   ],
   providers: [],
   bootstrap: [AppComponent]
